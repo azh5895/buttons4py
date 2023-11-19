a387db2a762f4a07926c51a1b55a3019ddd3fe99
azh5895 <151476077+azh5895@users.noreply.github.com>
2023-11-20 12:19:12 +1300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showArrow(ArrowNames.South)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showArrow(ArrowNames.North)
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
+basic.forever(function () {
+	
+})
